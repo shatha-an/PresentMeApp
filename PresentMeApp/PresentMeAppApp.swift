b80a59e71c8d9a85ce903610d905dd6b1662d3c7
shatha-an <shada-2013@hotmail.com>
2023-10-08 10:02:50 +0300

    Initial Commit

---
+++ PresentMeApp/PresentMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeAppApp.swift
+//  PresentMeApp
+//
+//  Created by Shatha Ajjaj on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
